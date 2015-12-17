7020d76622180cf57bd7386626a9d6ebe6f21ea4
ddarriba <ddarriba@udc.es>
2015-12-17 01:38:36 +0100

    updated new gui

--- src/gui/xmodeltest.cpp
+++ src/gui/xmodeltest.cpp
@@ -29,6 +29,8 @@ xmodeltest::xmodeltest(QWidget *parent) :
     ui->setupUi(this);
     status = st_active;
     enable(ui->tool_help, true);
+
+    on_radDatatypeDna_clicked();
     update_gui();
 
     /* Redirect Console output to QTextEdit */
@@ -44,6 +46,9 @@ xmodeltest::~xmodeltest()
 
 void xmodeltest::update_gui( void )
 {
+    char txt[30];
+    int n_model_sets, n_matrices, n_models;
+
     ui->act_open_tree->setEnabled(status & st_msa_loaded);
     ui->mnu_open_tree->setEnabled(status & st_msa_loaded);
     ui->act_open_parts->setEnabled(status & st_msa_loaded);
@@ -53,7 +58,67 @@ void xmodeltest::update_gui( void )
     enable(ui->tool_open_tree, status & st_msa_loaded, status & st_tree_loaded);
     enable(ui->tool_open_parts, status & st_msa_loaded, status & st_parts_loaded);
     enable(ui->tool_run_settings, status & st_msa_loaded, status & st_optimized);
+    enable(ui->tool_settings, !(status & st_optimized), ui->tool_settings->isChecked());
     enable(ui->tool_results, status & st_optimized);
+
+    ui->grpOptions->setVisible(ui->tool_settings->isChecked());
+    ui->grpConsoles->setVisible(!ui->tool_settings->isChecked());
+
+    /** SETTINGS **/
+    ui->modelsListView->setEnabled(
+                ui->radDatatypeProt->isChecked() ||
+                ui->radSetModelTest->isChecked() ||
+                ui->radSetPhyml->isChecked());
+    ui->grpSubstSchemes->setEnabled(
+                !ui->radDatatypeProt->isChecked() &&
+                (ui->radSetModelTest->isChecked() ||
+                ui->radSetPhyml->isChecked()));
+
+    ui->cbIModels->setEnabled( !ui->radSetRaxml->isChecked() );
+    ui->cbIGModels->setEnabled( !ui->radSetRaxml->isChecked() );
+
+    ui->sliderNCat->setEnabled( !ui->radSetRaxml->isChecked() &&
+                                ( ui->cbGModels->isChecked() ||
+                                  ui->cbIGModels->isChecked()));
+    ui->lblNCat->setEnabled(ui->sliderNCat->isEnabled());
+
+    ui->grpAdvanced->setVisible(ui->cbAdvanced->isChecked());
+    ui->modelsListView->setVisible(ui->cbShowMatrices->isChecked());
+
+    ui->lblNThreads->setText(QString::number(ui->sliderNThreads->value()));
+
+    n_model_sets = ui->cbNoRateVarModels->isChecked() +
+            ui->cbIModels->isChecked() +
+            ui->cbGModels->isChecked() +
+            ui->cbIGModels->isChecked();
+
+    n_matrices = 0;
+    if (ui->radDatatypeDna->isChecked() && ui->radSchemes203->isChecked())
+        n_matrices = N_DNA_ALLMATRIX_COUNT;
+    else
+        for (int i=0; i < ui->modelsListView->count(); i++)
+            if (ui->modelsListView->item(i)->checkState() == Qt::CheckState::Checked)
+                n_matrices++;
+
+    n_models = n_matrices * n_model_sets *
+            (ui->cbEqualFreq->isChecked() + ui->cbMlFreq->isChecked());
+    sprintf(txt, "%d", n_models);
+    ui->lblNumModels->setText(QString(txt));
+
+    if (n_models == 0)
+    {
+        ui->lblTextNModels->setStyleSheet("background-color:#f00;");
+        ui->lblNumModels->setStyleSheet("background-color:#f00;");
+    }
+    else
+    {
+        ui->lblTextNModels->setStyleSheet("");
+        ui->lblNumModels->setStyleSheet("");
+    }
+
+    int n_cats = ui->sliderNCat->value();
+    if (!(ui->cbGModels->isChecked() || ui->cbIGModels->isChecked()))
+        n_cats = 1;
 }
 
 static QString to_qstring(const char * msg, msg_level_id level)
@@ -127,4 +192,333 @@ void xmodeltest::on_tool_open_msa_triggered(QAction *arg1)
 void xmodeltest::on_tool_open_msa_released()
 {
     on_act_open_msa_triggered();
+}
+
+void xmodeltest::on_tool_settings_toggled(bool checked)
+{
+    update_gui();
+}
+
+
+
+
+/** SETTINGS **/
+
+void xmodeltest::autoSelectSchemes(const int schemes[], int n)
+{
+
+    for (int i=0; i < ui->modelsListView->count(); i++)
+        ui->modelsListView->item(i)->setCheckState(Qt::CheckState::Unchecked);
+
+    for (int i=0; i<n; i++)
+        ui->modelsListView->item(schemes[i])->setCheckState(Qt::CheckState::Checked);
+
+    update_gui();
+}
+
+void xmodeltest::on_sliderNCat_sliderMoved(int position)
+{
+    char txt[30];
+    sprintf(txt, "%d categories", position);
+    ui->lblNCat->setText(QString(txt));
+
+//    ui->lblEstimatedMem->setText(
+//                QString::number(
+//                    compute_size(position,
+//                                 ui->sliderNThreads->value())));
+}
+
+void xmodeltest::on_radSchemes3_clicked()
+{
+    if (ui->radDatatypeDna->isChecked())
+    {
+        int schemes[3] = {0, 1, 10};
+        autoSelectSchemes(schemes, 3);
+        ui->radSchemes3->setChecked(true);
+        // updateGUI();
+    }
+}
+
+void xmodeltest::on_radSchemes5_clicked()
+{
+    if (ui->radDatatypeDna->isChecked())
+    {
+        int schemes[5] = {0, 1, 2, 3, 10};
+        autoSelectSchemes(schemes, 5);
+        ui->radSchemes5->setChecked(true);
+        // updateGUI();
+    }
+}
+
+void xmodeltest::on_radSchemes7_clicked()
+{
+    if (ui->radDatatypeDna->isChecked())
+    {
+        int schemes[7] = {0, 1, 2, 3, 6, 9, 10};
+        autoSelectSchemes(schemes, 7);
+        ui->radSchemes7->setChecked(true);
+        //updateGUI();
+    }
+}
+
+void xmodeltest::on_radSchemes11_clicked()
+{
+    if (ui->radDatatypeDna->isChecked())
+    {
+        for (int i=0; i < ui->modelsListView->count(); i++)
+            ui->modelsListView->item(i)->setCheckState(Qt::CheckState::Checked);
+        ui->radSchemes11->setChecked(true);
+        // updateGUI();
+    }
+}
+
+void xmodeltest::on_radSchemes203_clicked()
+{
+    if (ui->radDatatypeDna->isChecked())
+    {
+        for (int i=0; i < ui->modelsListView->count(); i++)
+            ui->modelsListView->item(i)->setCheckState(Qt::CheckState::Unchecked);
+        ui->radSchemes203->setChecked(true);
+        update_gui();
+    }
+}
+
+//void xmodeltest::on_radTopoU_clicked()
+//{
+//    assert(ui->btnLoadTree->isEnabled());
+//    on_btnLoadTree_clicked();
+
+//    if (!utree_filename.compare(""))
+//    {
+//        ui->radTopoFixedGtr->setChecked(true);
+//    }
+//    updateGUI();
+//}
+
+//void xmodeltest::on_radTopoFixedMp_clicked()
+//{
+//    utree_filename = "";
+//    ui->lblTree->setStyleSheet("color: #007;");
+//    ui->lblTree->setText("Fixed Maximum Parsimony");
+//    updateGUI();
+//}
+
+//void xmodeltest::on_radTopoFixedGtr_clicked()
+//{
+//    utree_filename = "";
+//    ui->lblTree->setStyleSheet("color: #007;");
+//    ui->lblTree->setText("Fixed Maximum Likelihood (GTR)");
+//    updateGUI();
+//}
+
+//void xmodeltest::on_radTopoFixedJc_clicked()
+//{
+//    utree_filename = "";
+//    ui->lblTree->setStyleSheet("color: #007;");
+//    ui->lblTree->setText("Fixed Maximum Likelihood (JC)");
+//    updateGUI();
+//}
+
+//void xmodeltest::on_radTopoML_clicked()
+//{
+//    utree_filename = "";
+//    ui->lblTree->setStyleSheet("color: #007;");
+//    ui->lblTree->setText("Maximum Likelihood");
+//    updateGUI();
+//}
+
+void xmodeltest::on_radSetModelTest_clicked()
+{
+    on_radSetModelTest_toggled(true);
+}
+
+void xmodeltest::on_radSetMrbayes_clicked()
+{
+    on_radSetMrbayes_toggled(true);
+}
+
+void xmodeltest::on_radSetRaxml_clicked()
+{
+    on_radSetRaxml_toggled(true);
+}
+
+void xmodeltest::on_radSetPhyml_clicked()
+{
+    on_radSetPhyml_toggled(true);
+}
+
+void xmodeltest::on_radSetModelTest_toggled(bool checked)
+{
+    if (!checked)
+        return;
+    on_radSchemes11_clicked();
+    ui->cbMlFreq->setChecked(true);
+    ui->cbEqualFreq->setChecked(true);
+    ui->cbNoRateVarModels->setChecked(true);
+    ui->cbIModels->setChecked(true);
+    ui->cbGModels->setChecked(true);
+    ui->cbIGModels->setChecked(true);
+    update_gui();
+}
+
+void xmodeltest::on_radSetMrbayes_toggled(bool checked)
+{
+    if (!checked)
+        return;
+    on_radSchemes3_clicked();
+    ui->cbMlFreq->setChecked(true);
+    ui->cbEqualFreq->setChecked(true);
+    ui->cbNoRateVarModels->setChecked(true);
+    ui->cbGModels->setChecked(true);
+    ui->cbIModels->setChecked(true);
+    ui->cbIGModels->setChecked(true);
+    update_gui();
+}
+
+void xmodeltest::on_radSetRaxml_toggled(bool checked)
+{
+    if (!checked)
+        return;
+    on_radSchemes3_clicked();
+    ui->cbMlFreq->setChecked(true);
+    ui->cbEqualFreq->setChecked(true);
+    ui->cbNoRateVarModels->setChecked(false);
+    ui->cbGModels->setChecked(true);
+    ui->cbIModels->setChecked(false);
+    ui->cbIGModels->setChecked(false);
+    ui->sliderNCat->setValue(4);
+    on_sliderNCat_sliderMoved(4);
+    update_gui();
+}
+
+void xmodeltest::on_radSetPhyml_toggled(bool checked)
+{
+    if (!checked)
+        return;
+    on_radSchemes11_clicked();
+    ui->cbMlFreq->setChecked(true);
+    ui->cbEqualFreq->setChecked(true);
+    ui->cbNoRateVarModels->setChecked(true);
+    ui->cbIModels->setChecked(true);
+    ui->cbGModels->setChecked(true);
+    ui->cbIGModels->setChecked(true);
+    update_gui();
+}
+
+void xmodeltest::on_cbEqualFreq_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbMlFreq_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbNoRateVarModels_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbIModels_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbGModels_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbIGModels_toggled(bool checked)
+{
+    UNUSED(checked);
+    update_gui();
+}
+
+void xmodeltest::on_cbAdvanced_clicked()
+{
+    update_gui();
+}
+
+void xmodeltest::on_cbShowMatrices_clicked()
+{
+    update_gui();
+}
+
+void xmodeltest::on_radDatatypeDna_clicked()
+{
+    //ui->grpSubstSchemes->setVisible(true);
+    ui->grpSubstSchemes->setEnabled(true);
+    ui->cbMlFreq->setText("ML frequencies");
+    ui->modelsListView->clear();
+
+    ui->modelsListView->addItem("000000  JC / F81");
+    ui->modelsListView->addItem("010010  K80 / HKY85");
+    ui->modelsListView->addItem("010020  TrNef / TrN");
+    ui->modelsListView->addItem("012210  TPM1 / TPM1uf");
+    ui->modelsListView->addItem("010212  TPM2 / TPM2uf");
+    ui->modelsListView->addItem("012012  TPM3 / TPM3uf");
+    ui->modelsListView->addItem("012230  TIM1ef / TIM1");
+    ui->modelsListView->addItem("010232  TIM2ef / TIM2");
+    ui->modelsListView->addItem("012032  TIM3ef / TIM3");
+    ui->modelsListView->addItem("012314  TVMef / TVM");
+    ui->modelsListView->addItem("012345  SYM / GTR");
+
+    ui->modelsListView->setMinimumHeight(250);
+    ui->modelsListView->setMaximumHeight(250);
+    on_radSchemes11_clicked();
+}
+
+void xmodeltest::on_radDatatypeProt_clicked()
+{
+
+    //ui->grpSubstSchemes->setVisible(false);
+    ui->grpSubstSchemes->setEnabled(false);
+    ui->cbMlFreq->setText("Empirical frequencies");
+    ui->modelsListView->clear();
+    for (mt_index_t i=0; i<N_PROT_MODEL_MATRICES; i++)
+    {
+        ui->modelsListView->addItem(prot_model_names[i].c_str());
+        ui->modelsListView->item(i)->setCheckState(Qt::CheckState::Checked);
+    }
+
+    ui->modelsListView->setMinimumHeight(363);
+    ui->modelsListView->setMaximumHeight(363);
+    ui->modelsListView->selectAll();
+}
+
+void xmodeltest::on_radSchemesUser_clicked()
+{
+    ui->cbShowMatrices->setChecked(true);
+    on_cbShowMatrices_clicked();
+}
+
+void xmodeltest::on_modelsListView_itemClicked(QListWidgetItem *item)
+{
+    UNUSED(item);
+    ui->radSchemesUser->setChecked(true);
+    update_gui();
+}
+
+void xmodeltest::on_sliderNCat_valueChanged(int value)
+{
+    on_sliderNCat_sliderMoved(value);
+}
+
+void xmodeltest::on_sliderNThreads_valueChanged(int value)
+{
+    ui->lblNThreads->setText(QString::number(value));
+    int n_cats = ui->sliderNCat->value();
+    if (!(ui->cbGModels->isChecked() || ui->cbIGModels->isChecked()))
+        n_cats = 1;
+//    ui->lblEstimatedMem->setText(
+//                QString::number(
+//                    compute_size(n_cats,
+//                                 value)));
 }
--- src/gui/xmodeltest.h
+++ src/gui/xmodeltest.h
@@ -4,6 +4,7 @@
 #include "gui/qdebugstream.h"
 
 #include <QMainWindow>
+#include <QtGui/QListWidgetItem>
 
 enum msg_level_id {
     msg_lvl_error,
@@ -38,6 +39,42 @@ private slots:
     void on_tool_open_msa_triggered(QAction *arg1);
 
     void on_tool_open_msa_released();
+
+    void on_tool_settings_toggled(bool checked);
+
+    /** SETTINGS **/
+    void autoSelectSchemes(const int schemes[], int n);
+    void on_radSchemes3_clicked();
+    void on_radSchemes5_clicked();
+    void on_radSchemes7_clicked();
+    void on_radSchemes11_clicked();
+    void on_radSchemes203_clicked();
+
+    void on_radSetModelTest_clicked();
+    void on_radSetMrbayes_clicked();
+    void on_radSetRaxml_clicked();
+    void on_radSetPhyml_clicked();
+    void on_radSetModelTest_toggled(bool checked);
+    void on_radSetMrbayes_toggled(bool checked);
+    void on_radSetRaxml_toggled(bool checked);
+    void on_radSetPhyml_toggled(bool checked);
+
+    void on_cbEqualFreq_toggled(bool checked);
+    void on_cbMlFreq_toggled(bool checked);
+    void on_cbNoRateVarModels_toggled(bool checked);
+    void on_cbIModels_toggled(bool checked);
+    void on_cbGModels_toggled(bool checked);
+    void on_cbIGModels_toggled(bool checked);
+    void on_cbAdvanced_clicked();
+    void on_sliderNThreads_valueChanged(int value);
+    void on_radDatatypeDna_clicked();
+    void on_radDatatypeProt_clicked();
+    void on_cbShowMatrices_clicked();
+    void on_radSchemesUser_clicked();
+    void on_modelsListView_itemClicked(QListWidgetItem *item);
+
+    void on_sliderNCat_valueChanged(int value);
+    void on_sliderNCat_sliderMoved(int position);
 
 private:
     Ui::xmodeltest *ui;
